f2c43e50b6f4704ab494a75d4336d3b366dea242
rennand <rplaykl@gmail.com>
2021-02-18 23:27:51 -0300

    feat: Create test for create log with invalid type in log creation test

--- src/modules/APIClient/Services/CreateLogService.php
+++ src/modules/APIClient/Services/CreateLogService.php
@@ -16,12 +16,16 @@ class CreateLogService {
   public function run(int $status_code, string $type, $body) {
 
     $permitedStatus = [200,400,401,500];
+    $permitedTypes = ["error","success","test"];
     $httpResponse = new HttpResponseHelper();
 
     if(!in_array($status_code, $permitedStatus)) {
       return $httpResponse->badRequest('Invalid status code.');
     }
 
+    if(!in_array($type, $permitedTypes)) {
+      return $httpResponse->badRequest('Invalid type for the log.');
+    }
 
     return $httpResponse->success('Success');
   }
--- tests/CreateLogServiceTest.php
+++ tests/CreateLogServiceTest.php
@@ -3,19 +3,23 @@
 use Modules\APIClient\Services\CreateLogService;
 use Modules\APIClient\Fakes\FakeLogsRepository;
 
-class CreateLogServiceTest extends TestCase
-{
-  /**
-   * A basic test example.
-   *
-   * @return void
-   */
+class CreateLogServiceTest extends TestCase {
+
   public function testShouldNotBeAbleToCreateANewLogWithInvalidStatusCode(){
     $logRepository = new FakeLogsRepository();
     $createLog = new CreateLogService($logRepository);
 
     $log = $createLog->run(300,"error","message-error");
 
+
+    $this->assertEquals(400, $log['status_code']);
+  }
+
+  public function testShouldNotBeAbleToCreateANewLogWithInvalidYype(){
+    $logRepository = new FakeLogsRepository();
+    $createLog = new CreateLogService($logRepository);
+
+    $log = $createLog->run(200,"warning","message-test");
 
     $this->assertEquals(400, $log['status_code']);
   }
